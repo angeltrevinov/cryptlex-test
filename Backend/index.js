55bb22959d66293d3e2ca61bba33de3df46ef299
Angel Trevino <angel.trevinov@gmail.com>
2022-04-13 00:13:57 -0500

    fix

--- Backend/index.js
+++ Backend/index.js
@@ -23,12 +23,15 @@ const crytplexRoutes = require('./Routes/CryptlexRoutes');
 
 app.use('/cryptlex', crytplexRoutes);
 
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
 // START SERVER
 app.listen(process.env.PORT || '8080', () => {
     console.log('Server is running...');
 });
-
-const distDir = __dirname + "/build/";
-app.use(express.static(distDir));
 
 module.exports = app;
